65b6af56d5454ee11df85f56aa3206cfa18e8e6e
JediJeffatHype <75488088+JediJeffatHype@users.noreply.github.com>
2021-11-17 10:54:11 -0700

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const help = require ('discord.js')
+
+module.exports = {
+    name: 'help',
+    description: "a help command",
+    execute(message, args, cmd, client, Discord){
+    const help = new Discord.messageEmbed()
+    .setColor('#D2B48C')
+    .setTitle('List of Commands')
+    .addFields(
+        {name: 'Play', value: 'Do ~play and a song to play a song!'} 
+        )
+    }
+}
